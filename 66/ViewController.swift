c1a7bc8ca1f045c22e0614209e95767fa7f6e7a8
sy0201 <noey0201@gmail.com>
2024-11-25 11:28:53 +0900

    [feat] : commit test

--- 66/ViewController.swift
+++ 66/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 수정 siyeon
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
